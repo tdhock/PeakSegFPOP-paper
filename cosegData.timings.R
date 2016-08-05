b41870c9198c9377f2388ee81ec44f4173d83a7c
Toby Dylan Hocking <tdhock5@gmail.com>
2016-08-05 12:03:47 -0400

    lower limits based on gap track

--- cosegData.timings.R
+++ cosegData.timings.R
@@ -1,7 +1,124 @@
 data("McGill0003_H3K4me3_chr1", package="cosegData")
+data("hg19.gap", package="cosegData")
 library(coseg)
 library(memtime)
 library(data.table)
+
+last <- 1e5
+w <- 1e4
+l <- McGill0003_H3K4me3_chr1[last,]
+some <- McGill0003_H3K4me3_chr1[last+(-w):w,]
+ggplot()+
+  geom_vline(xintercept=l$chromStart/1e3)+
+  geom_step(aes(chromStart/1e3, count),
+          color="grey50",
+            data=some)
+
+## field	example	SQL type 	info 	description
+## bin 	585	smallint(6) 	range 	Indexing field to speed chromosome range queries.
+## chrom 	chr1	varchar(255) 	values 	Reference sequence chromosome or scaffold
+## chromStart 	0	int(10) unsigned 	range 	start position in chromosome
+## chromEnd 	10000	int(10) unsigned 	range 	end position in chromosome
+## ix 	1	int(11) 	range 	index count of this fragment (obsolete/useless)
+## n 	N	char(1) 	values 	'N' for gaps of known size, 'U' for gaps of unknown size
+## size 	10000	int(10) unsigned 	range 	size of gap
+## type 	telomere	varchar(255) 	values 	scaffold, contig, clone, fragment, etc.
+## bridge 	no	varchar(255) 	values 	yes, no, mrna, bacEndPair, etc.
+## colClasses <- c(
+##   bin="NULL",
+##   chrom="factor",
+##   chromStart="integer",
+##   chromEnd="integer",
+##   ix="NULL",
+##   n="NULL",
+##   size="integer",
+##   type="factor",
+##   bridge="factor")
+## hg19.gap <- read.table("~/Downloads/gap.txt.gz", colClasses=colClasses, col.names=names(colClasses))
+## save(hg19.gap, file="~/R/cosegData/data/hg19.gap.RData")
+## prompt(hg19.gap, file="~/R/cosegData/man/hg19.gap.Rd")
+
+chr1.gap <- data.table(hg19.gap)[chrom=="chr1",]
+chr1.cov <- data.table(McGill0003_H3K4me3_chr1)
+setkey(chr1.cov, chromStart, chromEnd)
+setkey(chr1.gap, chromStart, chromEnd)
+over.dt <- foverlaps(chr1.cov, chr1.gap, nomatch=0L)
+
+ggplot()+
+  geom_segment(aes(chromStart/1e3, 0, xend=chromEnd/1e3, yend=0),
+               data=chr1.gap)+
+  geom_point(aes(i.chromStart/1e3, count),
+             color="red",
+             data=over.dt)+
+  geom_point(aes(chromStart/1e3, 0),
+             shape=1,
+             data=chr1.gap)+
+  geom_rect(aes(xmin=i.chromStart/1e3, xmax=i.chromEnd/1e3,
+                ymin=0, ymax=count),
+            data=over.dt)
+
+non.zero <- over.dt[0 < count,]
+ggplot()+
+  geom_segment(aes(chromStart/1e3, 0, xend=chromEnd/1e3, yend=0),
+               data=chr1.gap)+
+  geom_point(aes(i.chromStart/1e3, count),
+             color="red",
+             data=non.zero)+
+  geom_point(aes(chromStart/1e3, 0),
+             shape=1,
+             data=chr1.gap)
+
+some.gaps <- chr1.gap[chromStart %in% non.zero$chromStart,]
+ggplot()+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "lines"))+
+  facet_wrap("chromStart",scales="free")+
+  geom_segment(aes(chromStart/1e3, 0, xend=chromEnd/1e3, yend=0),
+               data=some.gaps)+
+  geom_point(aes(i.chromStart/1e3, count),
+             color="red",
+             data=non.zero)+
+  geom_point(aes(chromStart/1e3, 0),
+             shape=1,
+             data=some.gaps)+
+  geom_rect(aes(xmin=i.chromStart/1e3, xmax=i.chromEnd/1e3,
+                ymin=0, ymax=count),
+            data=non.zero)
+
+mid <- 121485434
+w <- 1e6
+some <- chr1.cov[!(chromStart < mid-w | mid+w < chromEnd),]
+ggplot()+
+  coord_cartesian(ylim=c(0, 100))+
+  geom_step(aes(chromStart/1e3, count),
+            color="grey50",
+            data=some)+
+  geom_segment(aes(chromStart/1e3, 0, xend=chromEnd/1e3, yend=0),
+               size=2,
+               data=chr1.gap[13:15,])
+
+between.gaps <- chr1.gap[, data.table(problemStart=chromEnd[-.N], problemEnd=chromStart[-1])]
+between.gaps[, bases := problemEnd-problemStart]
+chr1.problems <- between.gaps[0 < bases,]
+setkey(chr1.problems, problemStart, problemEnd)
+chr1.problems.cov <- foverlaps(chr1.cov, chr1.problems, nomatch=0L)
+chr1.problems.cov[chromStart < problemStart,]
+chr1.problems.cov[problemEnd < chromEnd,]
+problem.stats <- chr1.problems.cov[, list(
+  sum.bases=sum(chromEnd-chromStart),
+  data=.N
+  ), by=.(problemStart, problemEnd)]
+problem.stats[, bases := problemEnd-problemStart]
+max(problem.stats$bases)
+max(problem.stats$data)
+
+all.between <-
+  data.table(hg19.gap)[, data.table(
+    problemStart=chromEnd[-.N],
+    problemEnd=chromStart[-1]), by=chrom]
+all.between[, max(problemEnd-problemStart)]
+
+N <- 1e5
 pen.info.list <- list()
 for(N in 10^seq(4, 6, by=0.5)){
   some <- McGill0003_H3K4me3_chr1[1:N,]
--- figure-cosegData-timings.R
+++ figure-cosegData-timings.R
@@ -29,10 +29,20 @@ ggplot()+
 
 cosegData.timings$data <- "observed"
 test.dt <- data.table(
-  N=c(1e4, 13623262, 249230603),
-  label=c(NA, "typical bedGraph file", "datum for every base"),
+  N=c(1e4, 4485563, 115591997),
+  label=c(NA, "bedGraph lines in biggest\nchr1 region with no gap", "bases in biggest\nregion with no gap"),
   data="extrapolated")
 
+ggplot()+
+  geom_segment(aes(problemStart/1e3, 1,
+                   xend=problemEnd/1e3, yend=1),
+               data=all.between[chrom=="chr4",])+
+  geom_point(aes(chromStart/1e3, 0,
+                   xend=chromEnd/1e3, yend=0),
+               data=subset(hg19.gap, chrom=="chr4"))+
+  geom_segment(aes(chromStart/1e3, 0,
+                   xend=chromEnd/1e3, yend=0),
+               data=subset(hg19.gap, chrom=="chr4"))
 
 cosegData.timings[, ratio := as.integer(N/lambda)]
 txt[, ratio := as.integer(N/lambda)]
@@ -40,36 +50,50 @@ ggplot()+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
   facet_grid(. ~ ratio)+
-  scale_y_log10(breaks=c(range(cosegData.timings$seconds), 1, 10, 60))+
+  scale_y_log10()+
   scale_x_log10("data points to segment")+
-  geom_text(aes(N*1.05, megabytes, label=peaks),
+  geom_text(aes(N*1.05, kilobytes, label=peaks),
              data=txt,
              vjust=1.5,
              hjust=0)+
-  geom_point(aes(N, megabytes, fill=log10(lambda)),
+  geom_point(aes(N, kilobytes, fill=log10(lambda)),
              shape=21,
              data=cosegData.timings)
 
-some.times <- cosegData.timings[ratio==1 & 1e5 <= N,]
-fit.mem <- lm(log(megabytes) ~ log(N), some.times)
-test.dt$pred.megabytes <- exp(predict(fit.mem, test.dt))
+ggplot()+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "lines"))+
+  scale_y_log10()+
+  scale_x_log10("data points to segment")+
+  geom_text(aes(N*1.05, kilobytes, label=peaks),
+             data=txt,
+             vjust=1.5,
+             hjust=0)+
+  geom_point(aes(N, kilobytes, fill=log10(lambda)),
+             shape=21,
+             data=cosegData.timings)
+
+some.times <- cosegData.timings
+fit.mem <- lm(log(kilobytes) ~ log(N), some.times)
+test.dt$pred.kilobytes <- exp(predict(fit.mem, test.dt))
 show.dt <- test.dt[-1,]
-divide <- 1024
+divide <- 1024*1024
 gg.mem <- ggplot()+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
-  scale_y_log10(breaks=as.integer(c(show.dt$pred.megabytes/divide, 1, 4, 12)))+
-  scale_x_log10(breaks=c(10^(5:6), show.dt$N), label=scales::comma)+
-  geom_line(aes(N, pred.megabytes/divide), data=test.dt)+
-  geom_point(aes(N, pred.megabytes/divide, fill=data),
+  scale_y_log10("memory usage (gigabytes)", breaks=as.integer(c(show.dt$pred.kilobytes/divide, 1, 4, 12)))+
+  scale_x_log10("N = data points to segment", breaks=c(10^(5:6), show.dt$N), label=scales::comma)+
+  geom_line(aes(N, pred.kilobytes/divide), data=test.dt)+
+  geom_point(aes(N, pred.kilobytes/divide, fill=data),
              shape=21,
              data=show.dt)+
-  geom_text(aes(N/1.2, pred.megabytes/divide, label=label),
+  geom_text(aes(N/2, pred.kilobytes/divide, label=label),
             hjust=1,
             data=show.dt)+
-  geom_point(aes(N, megabytes/divide),
+  geom_point(aes(N, kilobytes/divide, fill=data),
+             shape=21,
              data=some.times)
-pdf("figure-cosegData-timings-memory.pdf", h=5)
+pdf("figure-cosegData-timings-memory.pdf", h=4.5)
 print(gg.mem)
 dev.off()
 
@@ -96,15 +120,16 @@ test.dt$pred.seconds <- exp(predict(fit, test.dt))
 show.dt <- test.dt[-1,]
 divide <- 60*60
 gg.pred <- ggplot()+
-  ggtitle("predicted time to segment human chr1")+
+  ##ggtitle("predicted time to segment human chr1")+
   theme_bw()+
-  scale_y_log10("hours of computation", breaks=round(show.dt$pred.seconds/divide, 1))+
-  scale_x_log10("data points to segment", breaks=as.integer(show.dt$N), label=scales::comma)+
+  scale_y_log10("hours of computation", breaks=round(show.dt$pred.seconds/divide, 2))+
+  scale_x_log10("data points to segment",
+                breaks=c(1e5, 1e6, as.integer(show.dt$N)), label=scales::comma)+
   geom_line(aes(N, pred.seconds/divide), data=test.dt)+
   geom_point(aes(N, pred.seconds/divide, fill=data),
              shape=21,
              data=test.dt)+
-  geom_text(aes(N/1.2, pred.seconds/divide, label=label),
+  geom_text(aes(N/2, pred.seconds/divide, label=label),
             hjust=1,
             data=show.dt)+
   geom_point(aes(N, seconds/divide, fill=data),
